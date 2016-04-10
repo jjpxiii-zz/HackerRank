d28084bbfc2665b13143f8da5603ff4d3113403e
Steve Wozniak <jj@MacBook-Pro.local>
2016-04-10 20:03:41 +0200

    Oops

--- HackerRank.playground/Pages/DecentNumber.xcplaygroundpage/Contents.swift
+++ HackerRank.playground/Pages/DecentNumber.xcplaygroundpage/Contents.swift
@@ -38,43 +38,6 @@ for (var i = 1; i <= n[0]!; i+=1) {
     }
 }
 
-// read the integer n
-var n = Int(readLine()!)!
-
-func findSolution(n: Int) -> (x: Int, y:Int) {
-    var yp = n
-    var xp = 0
-    while (yp >= 0) {
-        if (xp%5 == 0 && yp%3 == 0) {
-            break
-        }
-        yp -= 1
-        xp += 1
-        
-    }
-    return (Int(xp), Int(yp))
-}
-
-
-for (var i = 1; i <= n; i+=1) {
-    // read array and map the elements to integer
-    var num = Int(readLine()!)!
-    if (num<3) {
-        print ("-1")
-    }
-    else {
-        let res = findSolution(num)
-        if (res.x == -1 || res.y == -1) {
-            print ("-1")
-        }
-        else {
-            print (String(count: res.y, repeatedValue: Character("5")) + String(count: res.x, repeatedValue: Character("3")))
-        }
-    }
-}
-
-
-
 
 
 
