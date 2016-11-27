669056ced423e3a1303e16e6467e9a2cc80303e7
Steve Wozniak <jj@MacBook-Pro.local>
2016-11-27 02:12:33 +0100

    Chuck Norris done

--- Codewars.playground/Pages/ChuckNorris.xcplaygroundpage/Contents.swift
+++ Codewars.playground/Pages/ChuckNorris.xcplaygroundpage/Contents.swift
@@ -2,12 +2,30 @@
 
 import Foundation
 
-var str = "Hello, playground"
+let MESSAGE = "Chuck Norris' keyboard has 2 keys: 0 and white space."
 
-let b: UInt8 = 67
+let chars = String(MESSAGE).utf8.map{ String(UInt8($0), radix:2) }
+var j = chars.joined(separator: "")
 
-print(Character(UnicodeScalar(b)))
+let someString = "asd.asABCDEFG.HI"
+let regexp = "0+|1+"
+let a = j.range(of:regexp, options: .regularExpression)
+var res = ""
+while j.characters.count > 0 {
+    if let range = j.range(of:regexp, options: .regularExpression)
+    {
+        let temp = j.substring(with:range)
+        if temp.hasPrefix("1")
+        {
+            res += "0 " + String(repeating: "0", count: temp.characters.count) + " "
+        }
+        else
+        {
+            res += "00 " + String(repeating: "0", count: temp.characters.count) + " "
+        }
+        j = j.substring(from:range.upperBound)
+    }
+}
 
-UnicodeScalar("C")
-String("CCA").utf8.map{ UInt8($0) }
-String(b, radix:2)
+print(res)
+
